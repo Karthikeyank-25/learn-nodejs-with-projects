5a1db797d756340939ce11f938f785919a4350b1
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-14 22:15:50 +0530

    NodeJs

---
+++ nodeJs.js
@@ -0,0 +1,11 @@
+// NodeJs :
+//          Node.js is an open-source, cross-platform JavaScript runtime that lets you run JavaScript code outside of a web browser, typically on the server side.
+
+// Key idea:
+//          - Normally, JavaScript runs in a browser (like Chrome, Firefox) to make web pages interactive.
+//          - Node.js uses the V8 JavaScript engine (from Chrome) but runs it on your computer/server so you can use JavaScript for backend development.
+
+//Why it’s popular:
+//          - Non-blocking, event-driven I/O → handles many requests efficiently.
+//          - Uses the same language (JavaScript) for both frontend & backend.
+//          - Huge ecosystem of libraries via npm (Node Package Manager).
