27f024bf145aac453d5bf2c6b17a16cbc19ed97f
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-16 08:57:17 +0530

    Using nodeJs

---
+++ using nodeJs.js
@@ -0,0 +1,23 @@
+// Using nodeJs :
+//      We are going to learn in this 'using node' file are
+//          - How to check the version of the node.
+//          - REPL using node.
+//          - How to code using node.
+
+// Check version :
+//      Go to the terminal and write below code :
+            node -v
+
+// REPL using node :
+//      REPL stands for Read Eval Print Loop.
+//      REPL is a computer environment where user's inputs are read and evaluated and then results are returned to the user.
+            node            // type 'node' in the terminal to enter into the node REPL.
+
+            ctrl + C        // this is used to getout off anything in the commandline.
+
+// On the REPL node in the terminal , we write javaScript code asusual in the outside of the browser.
+
+// Running jS file :
+//      In the REPL node , to run the javaScript fie.
+            cd ['c:/Users/karth/OneDrive/Desktop/Js-Roadmap/js-learning-journey/mini project - 2']  // First 'cd' to the folder location.
+            node [fileName.js]              // Then write node then file name. 
