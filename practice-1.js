c73ff55ac676daf597616367403a8e36c867b5c8
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-16 13:47:40 +0530

    Practice 'npm' package modules

---
+++ practice-1.js
@@ -0,0 +1,10 @@
+// Generate Random SuperHeroes Name :
+
+// After write command in terminal to install package :
+'npm install superheroes'
+
+// Import that package into file :
+import { randomSuperhero } from 'superheroes';
+
+// Write code to print the superheroes names in the console :
+console.log(`I am ${randomSuperhero()}!!`);
