7c614b458646bd02ba3089963b1e868095c56a41
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-16 13:17:46 +0530

    Install 'npm' and external node modules

---
+++ npm manager and install external node modules.js
@@ -0,0 +1,13 @@
+// Install NPM and use extarnal node modules:
+
+//          - Install NPM by write command in the terminal.
+                'npm init'
+
+//          - Install NPM package by write command in the terminal.
+                'npm install [packageName]'
+
+//          - Write code to import from packages.
+                'import [exportName] from [packageName]'
+
+//          - Then write code to use the coded-package for our uses.
+
